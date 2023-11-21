671bd96089edea983ab08dac189ae131a350810a
SkylineR34stage3 <artemschannel2@gmail.com>
2023-11-21 21:37:31 +0100

    button fix

--- embedWidget.js
+++ embedWidget.js
@@ -6,6 +6,23 @@
     cssLink.type = 'text/css';
     document.head.appendChild(cssLink);
 
+    // Get the container and set up the widget
+    var container = document.getElementById('wahbvGQmDxsA'); // The ID of the div where the widget will be placed
+    var widgetContent = `
+        <div id="car-widget">
+            <h2>Car Listings</h2>
+            <div id="filters">
+                <input type="text" id="car-make" placeholder="Enter car make">
+                <input type="text" id="year-range" placeholder="Enter year range (e.g., 2010-2020)">
+                <button id="search-button">Search</button>
+            </div>
+            <div id="car-listings">
+                <!-- Car listings will be displayed here -->
+            </div>
+        </div>
+    `;
+    container.innerHTML = widgetContent;
+
     // Load the JavaScript file
     var scriptTag = document.createElement('script');
     scriptTag.src = 'https://ilma-webwidget-mvp.vercel.app/script.js'; // Replace with the actual URL
@@ -13,22 +30,16 @@
 
     // Wait until the script is loaded before executing the rest of the code
     scriptTag.onload = function() {
-        // Get the container and set up the widget
-        var container = document.getElementById('wahbvGQmDxsA'); // The ID of the div where the widget will be placed
-        var widgetContent = `
-            <div id="car-widget">
-                <h2>Car Listings</h2>
-                <div id="filters">
-                    <input type="text" id="car-make" placeholder="Enter car make">
-                    <input type="text" id="year-range" placeholder="Enter year range (e.g., 2010-2020)">
-                    <button id="search-button">Search</button>
-                </div>
-                <div id="car-listings">
-                    <!-- Car listings will be displayed here -->
-                </div>
-            </div>
-        `; // Your widget's HTML structure
-        container.innerHTML = widgetContent;
+        // Attach event listener to the search button
+        var searchButton = document.getElementById('search-button');
+        searchButton.addEventListener('click', function() {
+            var carMake = document.getElementById('car-make').value;
+            var yearRange = document.getElementById('year-range').value;
+            var filters = {};
+            if (carMake) filters.carMakers = carMake;
+            if (yearRange) filters.yearRange = yearRange;
+            fetchCars(filters);
+        });
 
         // Apply filters if they are provided
         var carMake = container.getAttribute('data-make');
